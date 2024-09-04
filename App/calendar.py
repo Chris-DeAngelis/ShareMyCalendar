acc32928dc70382f795502ca14e91684153fea5f
Chris DeAngelis <12800772+Chris-DeAngelis@users.noreply.github.com>
2024-09-04 11:46:02 -0500

    Update calendar.py

--- App/calendar.py
+++ App/calendar.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from icalendar import Calendar #, Event, vCalAddress, vText
+#from icalendar import Calendar #, Event, vCalAddress, vText
 from datetime import datetime, timedelta
 
 ############## Page Setup ##############
